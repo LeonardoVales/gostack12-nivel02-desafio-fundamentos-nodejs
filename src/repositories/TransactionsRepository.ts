f6b1763375f6102d536d0ecadda1418256b54838
Leonardo Vales <leohenrique.vales@gmail.com>
2020-06-27 19:07:25 -0300

    Desafio transações

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,52 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (accumulator: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulator.income += transaction.value;
+            break;
+          case 'outcome':
+            accumulator.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
+
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  // O método create recebe as informações que o Service enviou
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    // O repositório envia os dados recebidos do Service para a Model
+    // A Model cria e devolve a informação
+    const transaction = new Transaction({
+      title,
+      value,
+      type,
+    });
+
+    // Armazena a transaction criada no array de transactions
+    this.transactions.push(transaction);
+
+    // Retorna a transaction criada
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,15 +1,44 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+// Crio a interface para tipar os dados que o service vai receber
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
+  // O repositório já está instanciado no construtor
   constructor(transactionsRepository: TransactionsRepository) {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  // No método, eu passo os parâmetros com a interface e informo qual é a saída
+  // Nesse caso a saída é uma TRANSACTION
+  public execute({ title, value, type }: Request): Transaction {
+    // Compara se o type possui qualquer um desses valores
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('Transaction type is invalid');
+    }
+
+    // valida se o valor de saída é maior do que eu tenho em caixa
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new Error('You do not have enough balance');
+    }
+
+    // O service recebe os dados da rota (da requisição né) e envia para o repositório cadastrar os dados
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
